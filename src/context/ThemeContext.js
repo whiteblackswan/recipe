58cfaaed94efc4882ea5105de59d991d7eb5b06b
Sergey Lebed <swansergey7@gmail.com>
2023-07-28 13:10:10 +0300

    fixed a bug

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,15 +3,16 @@ import { createContext, useReducer } from 'react'
 export const ThemeContext = createContext()
 
 const themeReducer = (state, action) => {
-  switch (action.type) {
-    case 'CHANGE_COLOR':
-      return { ...state, color: action.payload }
-	case 'CHANGE_MODE':
-		return {...state, mode: action.payload }
-		default: 
-			return;
-  }
-}
+	switch (action.type) {
+	  case 'CHANGE_COLOR':
+		 return { ...state, color: action.payload }
+	  case 'CHANGE_MODE':
+		 return { ...state, mode: action.payload }
+	  default:
+		 return state
+	}
+ }
+ 
 
 export function ThemeProvider({ children }) {
   const [state, dispatch] = useReducer(themeReducer, {
