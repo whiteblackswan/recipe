ce4bece2c2fd87eeb5533e13efac0c71130c39d7
Sergey Lebed <swansergey7@gmail.com>
2023-07-29 21:13:30 +0300

    Fetching a Firestore Document

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,31 +1,48 @@
-import "./Recipe.css";
-import { useParams } from "react-router-dom";
-import React from "react";
-import { useFetch } from "../../hooks/useFetch";
-import { useTheme } from "../../hooks/useTheme";
+import { useParams } from 'react-router-dom'
+import { useTheme } from '../../hooks/useTheme'
+import { useState, useEffect } from 'react'
+import { projectFirestore } from '../../firebase/config'
 
-function Recipe() {
-	const { id } = useParams()
-	const url = 'http://localhost:3000/recipes/' + id
-	const { error, isPending, data: recipe } = useFetch(url)
-	const { mode } = useTheme()
- 
-	return (
-	  <div className={`recipe ${mode}`}>
-		 {error && <p className="error">{error}</p>}
-		 {isPending && <p className="loading">Loading...</p>}
-		 {recipe && (
-			<>
-				<h2 className="page-title">{recipe.title}</h2>
-				<p>Takes {recipe.cookingTime} to cook.</p>
-				<ul>
-					{recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
-				</ul>
-				<p className="method">{recipe.method}</p>
-			</>
-		 )}
-	  </div>
-	)
+// styles
+import './Recipe.css'
+
+export default function Recipe() {
+  const { id } = useParams()
+  const { mode } = useTheme()
+
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+  const [recipe, setRecipe] = useState(null)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore.collection('recipes').doc(id).get().then(doc => {
+      if (doc.exists) {
+        setIsPending(false)
+        setRecipe(doc.data())
+      } else {
+        setIsPending(false)
+        setError(`Could not find that recipe`)
+      }
+    })
+
+  }, [id])
+
+  return (
+    <div className={`recipe ${mode}`}>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  )
 }
-
-export default Recipe;
